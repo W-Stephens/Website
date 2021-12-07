57e9cc1afc0945d38fffb5898166bc59ccb1b59f
W-Stephens <50682149+W-Stephens@users.noreply.github.com>
2021-12-06 18:52:19 -0600

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,10 +1,16 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:wstephensprojects.database.windows.net,1433; Database = ProjectGroup28", "WStephens", "{Group28admin}");
+    $conn = new PDO("sqlsrv:server = tcp:wstephensprojects.database.windows.net,1433; Database = ProjectGroup28", "WStephens", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "WStephens", "pwd" => "{your_password_here}", "Database" => "ProjectGroup28", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:wstephensprojects.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
