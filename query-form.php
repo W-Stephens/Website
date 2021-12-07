b6634e9c499998867464b9f62f7a13e53f2ed8f3
W-Stephens <50682149+W-Stephens@users.noreply.github.com>
2021-12-06 18:45:48 -0600

    Update query-form.php

--- query-form.php
+++ query-form.php
@@ -38,7 +38,21 @@ function selectStock($conn) {
 }
 
 function deleteBot($conn) {
+    
+    $query = "SELECT * FROM BOT";
+    if ($result = $mysqli->query($query)) {
+        
+            while($row = $result->fetch_assoc()) {
+            $field1name = $row["col1"];
+            $field2name = $row["col2"];
+            $field3name = $row["col3"];
+            $field4name = $row["col4"];
+            $field5name = $row["col5"];
+    }
 
+    /* free result set */
+    $result->free();
+}  
  
 }
 
