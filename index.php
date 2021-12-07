2eb9e66d36016e1c9ecd58a2ea49faad8ea14a34
W-Stephens <50682149+W-Stephens@users.noreply.github.com>
2021-12-06 18:39:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,6 @@
 require_once("dbConn.php");
 require_once("query-form.php");
 
-$sql = "SELECT * FROM STOCK";
-
-$query = $conn->query($sql);
-
-foreach ($query as $row) {
-    if (!$query) {
-        echo "bad query";
-        return;
-    }
-    echo $row['Ticker'] . '  ';
-}
-
-
 ?>
 
 <!doctype HTML>
