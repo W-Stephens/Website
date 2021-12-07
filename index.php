5b36e7523ec4e7c82672ff59e610288df9bd48f3
W-Stephens <50682149+W-Stephens@users.noreply.github.com>
2021-12-06 19:19:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
 
 
-
+require_once("dbConn.php");
 require_once("query-form.php");
 
-
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:wstephensprojects.database.windows.net,1433; Database = ProjectGroup28", "WStephens", "{Group28admin}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "WStephens", "pwd" => "{Group28admin}", "Database" => "ProjectGroup28", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:wstephensprojects.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $co
-
-
-$sql = "SELECT * FROM BOT";
-$result = $conn->query($sql);
-$conn->close(); 
 
 ?>
 
@@ -35,34 +15,7 @@ $conn->close();
     <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
-    <section>
-        <h1>TEST TEST</h1>
-        <!-- TABLE CONSTRUCTION-->
-        <table>
-            <tr>
-                <th>STOCK</th>
-                <thPrevious Prices</th>
-                <th>Ticker</th>
-                <th>Current Price</th>
-            </tr>
-            <!-- PHP CODE TO FETCH DATA FROM ROWS-->
-            <?php   // LOOP TILL END OF DATA 
-                while($rows=$result->fetch_assoc())
-                {
-             ?>
-            <tr>
-                <!--FETCHING DATA FROM EACH 
-                    ROW OF EVERY COLUMN-->
-                <td><?php echo $rows['Previous_Price_At'];?></td>
-                <td><?php echo $rows['Ticker'];?></td>
-                <td><?php echo $rows['Current_Price'];?></td>
 
-            </tr>
-            <?php
-                }
-             ?>
-        </table>
-    </section>
     <!--nav class="flex-container">
         <a href="./query-page.html"><div>Query Page</div></a>
         <a href="./function-management.html"><div>Function Management</div></a>
